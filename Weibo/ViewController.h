0b8fd1441491a204488bbada6182bdea52bb4957
BluesJiang <763400095@qq.com>
2015-11-30 14:08:20 +0800

    Initial Commit

---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by BluesJiang on 30/11/15.
+//  Copyright © 2015年 BluesJiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weibo
+//
+//  Created by BluesJiang on 30/11/15.
+//  Copyright © 2015年 BluesJiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
